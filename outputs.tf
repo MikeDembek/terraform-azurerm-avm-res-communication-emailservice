ed044d99cbb2d1be174f689b2ba79e17e398f1a0
Mike Dembek <mdembek@boyneresorts.com>
2025-09-10 14:25:14 -0600

    Added output for email communication service domains with detailed attributes

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,22 @@ output "resource_in_azurerm_schema" {
   description = "The resource of email communication service in azurerm schema"
   value       = local.azurerm_resource_body
 }
+
+output "domains" {
+  description = "All attributes of created email communication service domains"
+  value = {
+    for key, domain in azapi_resource.email_communication_service_domain :
+    key => {
+      id                 = domain.id
+      name               = domain.name
+      location           = domain.location
+      parent_id          = domain.parent_id
+      type               = domain.type
+      body               = domain.body
+      tags               = domain.tags
+      identity           = try(domain.identity, null)
+      response_export_values = try(domain.response_export_values, null)
+      output             = try(domain.output, null)
+    }
+  }
+}
